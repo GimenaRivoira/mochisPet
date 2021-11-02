cbae57c7e1d534333f86c79ea834a3fcec6d1d16
GimenaRivoira <gimenarivoira@gmail.com>
2021-11-02 20:41:23 -0300

    deleted innecessary files and add dotenv config

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
+
 module.exports ={
   "development": {
-    "username": "root",
-    "password": "root",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
     "database": "mochispet",
     "host": "127.0.0.1",
     "dialect": "mysql"
